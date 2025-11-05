8df355bb506fb9da3bc3411609803cb101e30ffb
hemantverma25 <hemantverma25@gmail.com>
2025-11-05 06:09:38 +0000

    updated terraform tfstate to s3

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "vermah-roche-terraform-bucket"
+    key            = "dev/vermah/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "vermah-roche-table1" # Optional, for state locking    
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
